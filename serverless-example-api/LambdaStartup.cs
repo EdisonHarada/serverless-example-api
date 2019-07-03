c0cb88a64194569f5e0b4887456261de9a2db2c4
Edison Harada <you@example.com>
2019-07-03 23:54:16 +0200

    First Commit

---
+++ serverless-example-api/LambdaStartup.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace serverless_example_api
+{
+    public class LambdaStartup : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
